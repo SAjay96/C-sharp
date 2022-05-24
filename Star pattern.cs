97dddf97e039ced49bb6ade87de5101310bed5cf
SAjay96 <97229211+SAjay96@users.noreply.github.com>
2022-05-25 00:00:58 +0530

    Create Star pattern.cs

---
+++ Star pattern.cs
@@ -0,0 +1,21 @@
+ class Stardecre
+    {
+	public static void main(String[] args)
+	{
+
+
+
+		for (int i = 4; i >= 0; i--)
+		{
+
+			for (int j = 0; j <= i; j++)
+			{
+
+                Console.WriteLine("*" + " ");
+			}
+
+            Console.WriteLine(); 
+		}
+	}
+}
+
