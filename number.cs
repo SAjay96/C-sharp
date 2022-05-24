ae769186ab00b5865b6de64580febee649ad5e61
SAjay96 <97229211+SAjay96@users.noreply.github.com>
2022-05-25 00:00:13 +0530

    Create number.cs

---
+++ number.cs
@@ -0,0 +1,17 @@
+class Number
+    {
+		public static void main(String[] args)
+		{
+
+			for (int i = 0; i < 5; i++)
+			{
+				int k = 1;
+				for (int j = 0; j <= i; j++)
+				{
+                Console.WriteLine(k + "");
+					k++;
+				}
+            Console.WriteLine(); 
+			}
+		}
+	}
