1271ea01095a1d81b9b1edf71d02737ed564cdb7
SAjay96 <97229211+SAjay96@users.noreply.github.com>
2022-05-24 23:56:56 +0530

    Create R star.cs

---
+++ R star.cs
@@ -0,0 +1,14 @@
+class Rstar
+{
+    public static void main(String[] args)
+    { 
+     for(int i=1;i<=5;i++)  
+    {  
+        for(int j=1;j<=i;j++)  
+        {
+                Console.WriteLine("* ");
+        }
+            Console.WriteLine();
+    }  
+    }
+}
